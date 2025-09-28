fb961762ee16d9cf212fb168b8de7f78b964c56a
Duy-Hieu Bui <duyhieu.bui@gmail.com>
2025-09-28 19:17:14 -0400

    DPLL: add implementation script for digital_pll_controller

---
+++ openroad/croc_culp_chip/digital_pll_controller/config.mk
@@ -0,0 +1,236 @@
+export YOSYS_EXE = $(shell which yosys)
+export OPENROAD_EXE = $(shell which openroad)
+
+export DESIGN_NAME = digital_pll_controller
+export TOP_DESIGN_NICKNAME = croc_culp_chip
+export DESIGN_NICKNAME = ${TOP_DESIGN_NICKNAME}_${DESIGN_NAME}
+
+export TRACK_OPTION    = 7t
+export POWER_OPTION    = 5v0
+export METAL_OPTION    = 5LM_1TM
+export KVALUE          = 9
+
+export PLATFORM        = gf180
+
+# export PLACE_PINS_ARGS = -min_distance 50.0 -corner_avoidance 50.0
+export PDK_PATH = $(PDK_ROOT)/$(PDK)
+export FAST_LIB_FILES = $(abspath $(PLATFORM_DIR)/lib/gf180mcu_fd_sc_mcu$(TRACK_OPTION)$(POWER_OPTION)__ff_n40C_3v60.lib.gz)
+export FAST_TEMPERATURE = -40c
+export FAST_VOLTAGE     = 3.6
+
+export SLOW_LIB_FILES = $(abspath $(PLATFORM_DIR)/lib/gf180mcu_fd_sc_mcu$(TRACK_OPTION)$(POWER_OPTION)__ss_125C_3v00.lib.gz)
+
+export SLOW_TEMPERATURE = 125c
+export SLOW_VOLTAGE     = 3.0
+
+export TYP_LIB_FILES = $(abspath $(PLATFORM_DIR)/lib/gf180mcu_fd_sc_mcu$(TRACK_OPTION)$(POWER_OPTION)__tt_025C_3v30.lib.gz)
+
+export TYP_TEMPERATURE = 25c
+export TYP_VOLTAGE     = 3.3
+
+
+export PDK_PATH = $(PDK_ROOT)/$(PDK)
+export ADDITIONAL_LEFS = $(PDK_PATH)/libs.ref/gf180mcu_fd_ip_sram/lef/gf180mcu_fd_ip_sram__sram256x8m8wm1.lef \
+	$(PDK_PATH)/libs.ref/gf180mcu_fd_ip_sram/lef/gf180mcu_fd_ip_sram__sram512x8m8wm1.lef
+
+export ADDITIONAL_LIBS = $(PDK_PATH)/libs.ref/gf180mcu_fd_io/lib/gf180mcu_fd_io__tt_025C_3v30.lib \
+		$(PDK_PATH)/libs.ref/gf180mcu_fd_ip_sram/lib/gf180mcu_fd_ip_sram__sram256x8m8wm1__tt_025C_5v00.lib \
+		$(PDK_PATH)/libs.ref/gf180mcu_fd_ip_sram/lib/gf180mcu_fd_ip_sram__sram512x8m8wm1__tt_025C_5v00.lib
+export ADDITIONAL_GDS = $(PDK_PATH)/libs.ref/gf180mcu_fd_ip_sram/gds/gf180mcu_fd_ip_sram__sram256x8m8wm1.gds \
+		$(PDK_PATH)/libs.ref/gf180mcu_fd_ip_sram/gds/gf180mcu_fd_ip_sram__sram512x8m8wm1.gds
+
+export SDC_FILE        = $(DESIGN_NAME)/constraints.sdc
+
+export MAX_ROUTING_LAYER = Metal4
+
+export PDN_TCL = $(DESIGN_NAME)/pdn.tcl
+export IO_CONSTRAINTS = $(DESIGN_NAME)/io.tcl
+
+
+export PLACE_DENSITY = 0.55
+
+export CORNERS = slow typ fast
+
+export CORE_UTILIZATION       = 40
+export CORE_ASPECT_RATIO      = 1
+export CORE_MARGIN            = 2
+
+ifeq ($(USE_FILL),1)
+export DESIGN_TYPE = CELL
+else
+export DESIGN_TYPE = CELL_NODEN
+endif
+
+export VERILOG_INCLUDE_DIRS = ../../rtl/apb/include \
+        ../../rtl/common_cells/include \
+        ../../rtl/cve2/include \
+        ../../rtl/obi/include \
+        ../../rtl/register_interface/include
+
+export VERILOG_DEFINES = -D TARGET_ASIC -D TARGET_GF180MCU -D TARGET_SYNTHESIS -D SYNTHESIS=1
+export SYNTH_HDL_FRONTEND = slang
+
+export VERILOG_FILES = ../../rtl/common_cells/binary_to_gray.sv \
+../../rtl/common_cells/cb_filter_pkg.sv \
+../../rtl/common_cells/cc_onehot.sv \
+../../rtl/common_cells/cdc_reset_ctrlr_pkg.sv \
+../../rtl/common_cells/cf_math_pkg.sv \
+../../rtl/common_cells/clk_int_div.sv \
+../../rtl/common_cells/credit_counter.sv \
+../../rtl/common_cells/delta_counter.sv \
+../../rtl/common_cells/ecc_pkg.sv \
+../../rtl/common_cells/edge_propagator_tx.sv \
+../../rtl/common_cells/exp_backoff.sv \
+../../rtl/common_cells/fifo_v3.sv \
+../../rtl/common_cells/gray_to_binary.sv \
+../../rtl/common_cells/isochronous_4phase_handshake.sv \
+../../rtl/common_cells/isochronous_spill_register.sv \
+../../rtl/common_cells/lfsr.sv \
+../../rtl/common_cells/lfsr_16bit.sv \
+../../rtl/common_cells/lfsr_8bit.sv \
+../../rtl/common_cells/lossy_valid_to_stream.sv \
+../../rtl/common_cells/mv_filter.sv \
+../../rtl/common_cells/onehot_to_bin.sv \
+../../rtl/common_cells/plru_tree.sv \
+../../rtl/common_cells/passthrough_stream_fifo.sv \
+../../rtl/common_cells/popcount.sv \
+../../rtl/common_cells/rr_arb_tree.sv \
+../../rtl/common_cells/rstgen_bypass.sv \
+../../rtl/common_cells/serial_deglitch.sv \
+../../rtl/common_cells/shift_reg.sv \
+../../rtl/common_cells/shift_reg_gated.sv \
+../../rtl/common_cells/spill_register_flushable.sv \
+../../rtl/common_cells/stream_demux.sv \
+../../rtl/common_cells/stream_filter.sv \
+../../rtl/common_cells/stream_fork.sv \
+../../rtl/common_cells/stream_intf.sv \
+../../rtl/common_cells/stream_join_dynamic.sv \
+../../rtl/common_cells/stream_mux.sv \
+../../rtl/common_cells/stream_throttle.sv \
+../../rtl/common_cells/sub_per_hash.sv \
+../../rtl/common_cells/sync.sv \
+../../rtl/common_cells/sync_wedge.sv \
+../../rtl/common_cells/unread.sv \
+../../rtl/common_cells/read.sv \
+../../rtl/common_cells/addr_decode_dync.sv \
+../../rtl/common_cells/cdc_2phase.sv \
+../../rtl/common_cells/cdc_4phase.sv \
+../../rtl/common_cells/clk_int_div_static.sv \
+../../rtl/common_cells/addr_decode.sv \
+../../rtl/common_cells/addr_decode_napot.sv \
+../../rtl/common_cells/multiaddr_decode.sv \
+../../rtl/common_cells/cb_filter.sv \
+../../rtl/common_cells/cdc_fifo_2phase.sv \
+../../rtl/common_cells/clk_mux_glitch_free.sv \
+../../rtl/common_cells/counter.sv \
+../../rtl/common_cells/ecc_decode.sv \
+../../rtl/common_cells/ecc_encode.sv \
+../../rtl/common_cells/edge_detect.sv \
+../../rtl/common_cells/lzc.sv \
+../../rtl/common_cells/max_counter.sv \
+../../rtl/common_cells/rstgen.sv \
+../../rtl/common_cells/spill_register.sv \
+../../rtl/common_cells/stream_delay.sv \
+../../rtl/common_cells/stream_fifo.sv \
+../../rtl/common_cells/stream_fork_dynamic.sv \
+../../rtl/common_cells/stream_join.sv \
+../../rtl/common_cells/cdc_reset_ctrlr.sv \
+../../rtl/common_cells/cdc_fifo_gray.sv \
+../../rtl/common_cells/fall_through_register.sv \
+../../rtl/common_cells/id_queue.sv \
+../../rtl/common_cells/stream_to_mem.sv \
+../../rtl/common_cells/stream_arbiter_flushable.sv \
+../../rtl/common_cells/stream_fifo_optimal_wrap.sv \
+../../rtl/common_cells/stream_register.sv \
+../../rtl/common_cells/stream_xbar.sv \
+../../rtl/common_cells/cdc_fifo_gray_clearable.sv \
+../../rtl/common_cells/cdc_2phase_clearable.sv \
+../../rtl/common_cells/mem_to_banks_detailed.sv \
+../../rtl/common_cells/stream_arbiter.sv \
+../../rtl/common_cells/stream_omega_net.sv \
+../../rtl/common_cells/mem_to_banks.sv \
+../../rtl/apb/apb_pkg.sv \
+../../rtl/obi/obi_pkg.sv \
+../../rtl/obi/obi_intf.sv \
+../../rtl/obi/obi_rready_converter.sv \
+../../rtl/obi/obi_atop_resolver.sv \
+../../rtl/obi/obi_cut.sv \
+../../rtl/obi/obi_demux.sv \
+../../rtl/obi/obi_err_sbr.sv \
+../../rtl/obi/obi_mux.sv \
+../../rtl/obi/obi_sram_shim.sv \
+../../rtl/obi/obi_xbar.sv \
+../../rtl/cve2/cve2_pkg.sv \
+../../rtl/cve2/cve2_alu.sv \
+../../rtl/cve2/cve2_compressed_decoder.sv \
+../../rtl/cve2/cve2_controller.sv \
+../../rtl/cve2/cve2_counter.sv \
+../../rtl/cve2/cve2_csr.sv \
+../../rtl/cve2/cve2_decoder.sv \
+../../rtl/cve2/cve2_fetch_fifo.sv \
+../../rtl/cve2/cve2_load_store_unit.sv \
+../../rtl/cve2/cve2_multdiv_fast.sv \
+../../rtl/cve2/cve2_multdiv_slow.sv \
+../../rtl/cve2/cve2_pmp.sv \
+../../rtl/cve2/cve2_register_file_ff.sv \
+../../rtl/cve2/cve2_wb.sv \
+../../rtl/cve2/cve2_cs_registers.sv \
+../../rtl/cve2/cve2_ex_block.sv \
+../../rtl/cve2/cve2_id_stage.sv \
+../../rtl/cve2/cve2_prefetch_buffer.sv \
+../../rtl/cve2/cve2_if_stage.sv \
+../../rtl/cve2/cve2_core.sv \
+../../rtl/user_domain/dpll/RTL/obi_dpll_pkg.sv \
+../../rtl/user_domain/dpll/RTL/obi_dpll.sv \
+../../rtl/user_domain/dpll/RTL/obi_dpll_register.sv \
+../../rtl/obi_uart/obi_uart_pkg.sv \
+../../rtl/obi_uart/obi_uart_baudgen.sv \
+../../rtl/obi_uart/obi_uart_interrupts.sv \
+../../rtl/obi_uart/obi_uart_modem.sv \
+../../rtl/obi_uart/obi_uart_rx.sv \
+../../rtl/obi_uart/obi_uart_tx.sv \
+../../rtl/obi_uart/obi_uart_register.sv \
+../../rtl/obi_uart/obi_uart.sv \
+../../rtl/register_interface/reg_intf.sv \
+../../rtl/register_interface/lowrisc_opentitan/prim_subreg_arb.sv \
+../../rtl/register_interface/lowrisc_opentitan/prim_subreg_ext.sv \
+../../rtl/register_interface/periph_to_reg.sv \
+../../rtl/register_interface/reg_to_apb.sv \
+../../rtl/register_interface/lowrisc_opentitan/prim_subreg_shadow.sv \
+../../rtl/register_interface/lowrisc_opentitan/prim_subreg.sv \
+../../rtl/riscv-dbg/dm_pkg.sv \
+../../rtl/riscv-dbg/debug_rom/debug_rom.sv \
+../../rtl/riscv-dbg/debug_rom/debug_rom_one_scratch.sv \
+../../rtl/riscv-dbg/dm_csrs.sv \
+../../rtl/riscv-dbg/dm_mem.sv \
+../../rtl/riscv-dbg/dmi_cdc.sv \
+../../rtl/riscv-dbg/dmi_jtag_tap.sv \
+../../rtl/riscv-dbg/dm_sba.sv \
+../../rtl/riscv-dbg/dm_top.sv \
+../../rtl/riscv-dbg/dmi_jtag.sv \
+../../rtl/riscv-dbg/dm_obi_top.sv \
+../../rtl/timer_unit/timer_unit_counter.sv \
+../../rtl/timer_unit/timer_unit_counter_presc.sv \
+../../rtl/timer_unit/apb_timer_unit.sv \
+../../rtl/timer_unit/timer_unit.sv \
+../../gf180mcu/tc_clk.sv \
+../../gf180mcu/gf180mcu_sram_wrapper.sv \
+../../gf180mcu/tc_sram_impl.sv \
+../../rtl/croc_pkg.sv \
+../../rtl/user_pkg.sv \
+../../rtl/soc_ctrl/soc_ctrl_reg_pkg.sv \
+../../rtl/gpio/gpio_reg_pkg.sv \
+../../rtl/core_wrap.sv \
+../../rtl/soc_ctrl/soc_ctrl_reg_top.sv \
+../../rtl/gpio/gpio_reg_top.sv \
+../../rtl/gpio/gpio.sv \
+../../rtl/user_domain/dpll/RTL/digital_pll.v \
+../../rtl/user_domain/dpll/RTL/digital_pll_controller.v \
+../../rtl/user_domain/dpll/RTL/ring_osc2x13.v \
+../../rtl/croc_domain.sv \
+../../rtl/user_domain.sv \
+../../rtl/croc_soc.sv \
+../../rtl/croc_chip.sv \
+../../rtl/croc_culp.sv \
+../../rtl/croc_padring.sv \
+../../rtl/croc_culp_chip.sv
---
+++ openroad/croc_culp_chip/digital_pll_controller/constraints.sdc
@@ -0,0 +1,24 @@
+set CLK_SYS 8
+create_clock -name clk_sys -period $CLK_SYS [get_ports clock]
+
+create_clock -name osc_clk -period 10 [get_ports osc]
+
+set_clock_groups -asynchronous -name clk_groups_async \
+     -group {clk_sys} \
+     -group {osc_clk}
+
+# set_false_path -from [get_ports reset]
+set_clock_uncertainty 0.1 [all_clocks]
+set_clock_transition  0.2 [all_clocks]
+
+set_driving_cell [all_inputs] -lib_cell gf180mcu_fd_sc_mcu7t5v0__buf_4 -pin Z
+set_load 0.015 [all_outputs]
+
+set_input_delay  -min -add_delay -clock clk_sys [ expr $CLK_SYS * 0.10 ] [get_ports {reset enable dco div*}]
+set_input_delay  -max -add_delay -clock clk_sys [ expr $CLK_SYS * 0.30 ] [get_ports {reset enable dco div*}]
+
+set_output_delay -min -add_delay -clock clk_sys [ expr $CLK_SYS * 0.10 ] [get_ports {trim*}]
+set_output_delay -max -add_delay -clock clk_sys [ expr $CLK_SYS * 0.30 ] [get_ports {trim*}]
+
+set_timing_derate -early 0.9500
+set_timing_derate -late 1.0500
