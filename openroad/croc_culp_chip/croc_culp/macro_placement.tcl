bc765eaf87ef49aff644c75a29b8870d389f964a
Duy-Hieu Bui <duyhieu.bui@gmail.com>
2025-10-01 17:05:39 -0400

    DRC: fix antenna violation by moving ring_osc and change placement density in croc_culp

--- openroad/croc_culp_chip/croc_culp/macro_placement.tcl
+++ openroad/croc_culp_chip/croc_culp/macro_placement.tcl
@@ -1,15 +1,14 @@
-place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[0\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[1\].depth_bank\[0\].genblk1.u_sram} -location {48.0 1400.72} -orientation MY
-place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[0\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[2\].depth_bank\[0\].genblk1.u_sram} -location {541.08 1400.72} -orientation R0
+place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[1\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[0\].depth_bank\[0\].genblk1.u_sram} -location {48.16 1379.84} -orientation MY
+place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[1\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[2\].depth_bank\[0\].genblk1.u_sram} -location {540.96 1379.84} -orientation R0
+place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[0\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[3\].depth_bank\[0\].genblk1.u_sram} -location {1655.92 1379.84} -orientation R0
+place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[1\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[3\].depth_bank\[0\].genblk1.u_sram} -location {1121.12 1379.84} -orientation R0
 
-place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[1\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[1\].depth_bank\[0\].genblk1.u_sram} -location {1655.96 1400.72} -orientation R0
-
-place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[1\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[3\].depth_bank\[0\].genblk1.u_sram} -location {1121.0 1400.72} -orientation R0
 ## cluster
-place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[1\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[0\].depth_bank\[0\].genblk1.u_sram} -location {48.0 35.68} -orientation MX
-place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[0\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[3\].depth_bank\[0\].genblk1.u_sram} -location {1655.96 35.68} -orientation MX
+place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[0\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[1\].depth_bank\[0\].genblk1.u_sram} -location {48.16 39.2} -orientation MX
+place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[0\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[2\].depth_bank\[0\].genblk1.u_sram} -location {540.96 39.2} -orientation MX
+place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[0\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[0\].depth_bank\[0\].genblk1.u_sram} -location {1121.12 39.2} -orientation MX
+place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[1\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[1\].depth_bank\[0\].genblk1.u_sram} -location {1655.92 39.2} -orientation MX
 
-place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[0\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[0\].depth_bank\[0\].genblk1.u_sram} -location {1121.0 35.68} -orientation MX
-place_macro -macro_name {i_croc_soc.i_croc.gen_sram_bank\[1\].i_sram.gen_512x32xBx1.i_cut.byte_slice\[2\].depth_bank\[0\].genblk1.u_sram} -location {541.08 35.68} -orientation MX
 
 # place_macro -macro_name {i_croc_soc/i_croc/gen_sram_bank\[0\].i_sram/gen_512x32xBx1.i_cut.byte_slice\[0\].depth_bank\[0\].genblk1.u_sram} -location {45.56 35.68} -orientation R180
 # place_macro -macro_name {i_croc_soc/i_croc/gen_sram_bank\[0\].i_sram/gen_512x32xBx1.i_cut.byte_slice\[1\].depth_bank\[0\].genblk1.u_sram} -location {540.6 35.68} -orientation R180
--- openroad/croc_culp_chip/ring_osc2x13/io.tcl
+++ openroad/croc_culp_chip/ring_osc2x13/io.tcl
@@ -1,1 +1,4 @@
-exclude_io_pin_region -region bottom:* -region left:* -region right:*
+exclude_io_pin_region -region left:* -region right:*
+set_io_pin_constraint -pin_names {reset enable dco ext_trim* trim* } -region top:*
+set_io_pin_constraint -pin_names { clockp*  } -region bottom:*
+
