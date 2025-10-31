0349c16f8c05ae807b88c7f5e8dae24b1377eb7b
Duy-Hieu Bui <duyhieu.bui@gmail.com>
2025-10-31 18:31:28 -0400

    OPENROAD: Fix routing over the pad

--- openroad/croc_culp_chip/add_routing_blk.tcl
+++ openroad/croc_culp_chip/add_routing_blk.tcl
@@ -29,8 +29,8 @@ foreach inst $allInsts {
     set llx_Mx [expr $loc_llx]
     set lly_Mx [expr $loc_lly]
       if { [string match "*R90" $orient] } {
-    	  set urx_Mx [expr $loc_llx + $h]
-    	  set ury_Mx [expr $loc_lly + $w]	  
+	  set urx_Mx [expr $loc_llx + $h]
+	  set ury_Mx [expr $loc_lly + $w]
       } else {
 	  set urx_Mx [expr $loc_llx + $w]
 	  set ury_Mx [expr $loc_lly + $h]
@@ -68,6 +68,47 @@ foreach inst $allInsts {
     set obs_M5 [odb::dbObstruction_create $block $layer_M5 $llx_Mx $lly_Mx $urx_Mx $ury_Mx]
 
     incr cnt
+  } elseif { [string match "gf180mcu_fd_io__dv*" $name] } {
+    set w [$master getWidth]
+    set h [$master getHeight]
+      set orient [$inst getOrient]
+      puts "bi: $orient"
+    ## left side
+    set llx_Mx [expr $loc_llx]
+    set lly_Mx [expr $loc_lly]
+    if { [string match "*R90" $orient] } {
+	set urx_Mx [expr $loc_llx + $h]
+	set ury_Mx [expr $loc_lly + $w]
+	puts "in: $orient $llx_Mx $lly_Mx $urx_Mx $ury_Mx"
+	set obs_M2 [odb::dbObstruction_create $block $layer_M2 $llx_Mx $lly_Mx $urx_Mx $ury_Mx]
+	set obs_M3 [odb::dbObstruction_create $block $layer_M3 $llx_Mx $lly_Mx $urx_Mx $ury_Mx]
+	set obs_M4 [odb::dbObstruction_create $block $layer_M4 $llx_Mx $lly_Mx $urx_Mx $ury_Mx]
+	set obs_M5 [odb::dbObstruction_create $block $layer_M5 $llx_Mx $lly_Mx $urx_Mx $ury_Mx]
+
+	incr cnt
+    }
+  } elseif { [string match "gf180mcu_fd_io__fill*" $name] } {
+    set w [$master getWidth]
+    set h [$master getHeight]
+      set orient [$inst getOrient]
+      puts "bi: $orient"
+    ## left side
+    set llx_Mx [expr $loc_llx]
+    set lly_Mx [expr $loc_lly]
+    if { [string match "*R90" $orient] } {
+	set urx_Mx [expr $loc_llx + $h]
+	set ury_Mx [expr $loc_lly + $w]
+    } else {
+	set urx_Mx [expr $loc_llx + $w]
+	set ury_Mx [expr $loc_lly + $h]
+    }
+      puts "in: $orient $llx_Mx $lly_Mx $urx_Mx $ury_Mx"
+      set obs_M2 [odb::dbObstruction_create $block $layer_M2 $llx_Mx $lly_Mx $urx_Mx $ury_Mx]
+      set obs_M3 [odb::dbObstruction_create $block $layer_M3 $llx_Mx $lly_Mx $urx_Mx $ury_Mx]
+      set obs_M4 [odb::dbObstruction_create $block $layer_M4 $llx_Mx $lly_Mx $urx_Mx $ury_Mx]
+      set obs_M5 [odb::dbObstruction_create $block $layer_M5 $llx_Mx $lly_Mx $urx_Mx $ury_Mx]
+
+      incr cnt
   }
 }
 
